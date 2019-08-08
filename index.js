e485113256a4fb882f339b62b09018b0d123b8f8
Niten001 <timartin1069@gmail.com>
2019-08-08 14:39:04 +1000

    Update server backend and restructure

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
 
-var server = http.createServer(function (request, response) {
-    console.log(request.url);
-    if (request.url == "/") {
-        fs.readFile('./index.html', function(err, data) {
-            response.setHeader('Content-type', 'text/html');
-            response.write(data);
-            response.end();
-        });
-    } else {
-        fs.readFile('./' + request.url, function(err, data) {
-            if (!err) {
-                var dotoffset = request.url.lastIndexOf('.');
-                var mimetype = dotoffset == -1
-                                ? 'text/plain'
-                                : {
-                                    '.html' : 'text/html',
-                                    '.ico' : 'image/x-icon',
-                                    '.jpg' : 'image/jpeg',
-                                    '.png' : 'image/png',
-                                    '.gif' : 'image/gif',
-                                    '.css' : 'text/css',
-                                    '.js' : 'text/javascript',
-                                    '.ttf' : 'font/ttf',
-                                    '.woff2' : 'font/woff2',
-                                    '.woff' : 'font/woff',
-                                    '.eot' : 'font/eot',
-                                    '.svg' : 'image/svg+xml',
-                                    '.map' : 'text-plain'
-                                    }[ request.url.substr(dotoffset) ];
-                response.setHeader('Content-type' , mimetype);
-                response.end(data);
-            } else {
-                response.writeHead(404, "Not Found");
-                response.end();
-            }
-        });
-    }
-}).listen(8080);
+app.use('/', express.static('./html'));
+app.use('/css', express.static('./css'));
+app.use('/fonts', express.static('./fonts'));
+app.use('/js', express.static('./js'));
+app.use('/img', express.static('./img'));
+
+app.get('/', (req, res) => {
+    res.redirect('./index.html');
+});
+
+app.use(parser.urlencoded({ extender: true }));
+
+app.listen(8080);
+console.log('Running on http://localhost:8080');
