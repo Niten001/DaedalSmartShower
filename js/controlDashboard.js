1fa8d86e9082dca03ecdc0c6c3528386aa0f0a46
tbattyesmith <thomas_battye-smith@hotmail.com>
2019-08-21 17:59:19 +1000

    The StartStop function. Tom

--- js/controlDashboard.js
+++ js/controlDashboard.js
@@ -25,3 +25,37 @@ function setMenuSignUpState() {
     document.getElementsByClassName("signInFormMenu")[0].id = "unselectedSignIn";
     document.getElementsByClassName("signUpFormMenu")[0].id = "selectedState";
 }
+
+var button = 0;
+var clearTime;
+
+function startStop() {
+  var initialTime = (new Date).getTime();
+  var currentTime;
+  var runTime;
+  var seconds;
+
+  if (button == 1)
+  {
+    button = 0;
+    clearInterval(clearTime);
+    document.getElementById("time_container_start").innerHTML = "Start";
+    document.getElementById("time_container").children[0].innerHTML = "0:00:00";
+  }
+  else
+  {
+    button = 1;
+    document.getElementById("time_container_start").innerHTML = "Stop";
+
+    clearTime = setInterval(function(){
+    currentTime = (new Date).getTime();
+    runTime = new Date(currentTime-initialTime);
+    seconds = runTime.getSeconds();
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    document.getElementById("time_container").children[0].innerHTML = runTime.getMinutes() + ':' + seconds;
+    },1000);
+  }
+}
---
+++ js/dataDashboard.js
@@ -0,0 +1,3 @@
+function goToControls() {
+    window.location.href = "./index.html";
+}
